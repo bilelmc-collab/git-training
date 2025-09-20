9a01b97a3b1c1972171e383d90fcd7957f3c0269
bilel-dev <bilel.daikhi@gmail.com>
2025-09-20 19:31:36 +0100

    second commit again

--- src/main/java/com/git/training/GitTrainingApplication.java
+++ src/main/java/com/git/training/GitTrainingApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitTrainingApplication {
 
     public static void main(String[] args) {
+        System.out.println("change 2!");
+        System.out.println("change 3!");
+
         SpringApplication.run(GitTrainingApplication.class, args);
     }
 
