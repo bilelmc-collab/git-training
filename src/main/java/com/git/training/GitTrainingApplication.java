e49fbf2798f75fcb6debc419600a8baab3bd44ad
bilelmc-collab <bilelmc@gmail.com>
2025-09-20 20:30:48 +0100

    Update GitTrainingApplication.java change 8 remote repo

--- src/main/java/com/git/training/GitTrainingApplication.java
+++ src/main/java/com/git/training/GitTrainingApplication.java
@@ -14,6 +14,7 @@ public class GitTrainingApplication {
         System.out.println("change 6 in new branch num 1!");
         System.out.println("change 7 in new branch num 1 change from remote repository!!");
         System.out.println("change 7 in new branch num 1 change from local repository!");
+        System.out.println("change 8 in new branch num 1 change from remote repository!");
         SpringApplication.run(GitTrainingApplication.class, args);
     }
 
